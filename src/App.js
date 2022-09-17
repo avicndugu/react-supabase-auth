e0709b2d30db5de1f501054edeef00d3594ab2d4
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-09-17 16:03:25 +0300

    Execute signup, login, logout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+// Initialize the Supabase JS client
+import { createClient } from '@supabase/supabase-js';
+
 
 function App() {
+  const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+  const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+  const signInWithEmail = async () => {
+    const { user, error } = await supabase.auth.signInWithPassword({
+      email: 'avicndugu1234@gmail.com',
+      password: 'example-password',
+    })
+  }
+
+  // Sign up function works
+  const signUpWithEmail = async () => {
+    const { user, session, error } = await supabase.auth.signUp({
+      email: 'avicndugu1234@gmail.com',
+      password: 'example-password',
+    })
+    await console.log(user)
+  }
+  // Sign out function works. Deletes the entry on the localstorage
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=> signUpWithEmail() }>Sign Up Button</button>
+      <button onClick={()=> signInWithEmail() }>Sign In Button</button>
+      <button onClick={()=> signOut() }>Sign Out Button</button>
+
     </div>
   );
 }
