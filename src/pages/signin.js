34033db4050c5dfb1a406da1999e3013c681c212
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-09-21 14:55:00 +0300

    Show dashboard after signing in

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,12 @@
+import { useState, useEffect } from 'react';
 import { supabase } from '../supabase';
 import { Link } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
+
 
 export default function SignIn(props){
+  const [signInState, setSignInState] = useState(false);
+
   const signInWithEmail = async () => {
     const { user, error } = await supabase.auth.signInWithPassword({
       email: 'avicndugu1234@gmail.com',
@@ -10,38 +15,46 @@ export default function SignIn(props){
   }
   // Listen for the signed in event
   supabase.auth.onAuthStateChange((event, session) => {
-    if (event == 'SIGNED_IN') console.log('SIGNED_IN', session)
+    if (event == 'SIGNED_IN') {
+      console.log('SIGNED_IN', session)
+      setSignInState(true)
+    }
   })
+
   const signUpState = () => {
     signInWithEmail();
     console.log("user");
   }
-  return (
-    <>
-      <div className="navbar" style={{display:"flex", justifyContent: "space-between"}}>
-        <Link to="/"><button>Home</button></Link> 
-        <Link to="/signup"><button>Sign Up</button></Link>
-      </div>
-      <div className="pt-1" style={{background:"#ede"}}>
-      <p style={{textAlign:"center"}}><b>Sign In</b></p>
-        <div className="container pt-1 pb-1">
-          <div className="pb-1">
-            <label htmlFor="related-images-en">Email</label>
-            <br />
-            <br />
-            <input type="text" id="related-images-en" />
-            <br />
-            <br />
-            <label htmlFor="related-images-en">Password</label>
-            <br />
-            <br />
-            <input type="text" id="related-images-en" />
-            <br />
-            <br />
-            <button onClick={()=> signUpState() }>Sign In Button</button>
+  if(!signInState){
+    return (
+      <>
+        <div className="navbar" style={{display:"flex", justifyContent: "space-between"}}>
+          <Link to="/"><button>Home</button></Link> 
+          <Link to="/signup"><button>Sign Up</button></Link>
+        </div>
+        <div className="pt-1" style={{background:"#ede"}}>
+        <p style={{textAlign:"center"}}><b>Sign In</b></p>
+          <div className="container pt-1 pb-1">
+            <div className="pb-1">
+              <label htmlFor="related-images-en">Email</label>
+              <br />
+              <br />
+              <input type="text" id="related-images-en" />
+              <br />
+              <br />
+              <label htmlFor="related-images-en">Password</label>
+              <br />
+              <br />
+              <input type="text" id="related-images-en" />
+              <br />
+              <br />
+              <button onClick={()=> signUpState() }>Sign In Button</button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  } else {
+    return <Navigate to="/dashboard" />;
+  }
 }
